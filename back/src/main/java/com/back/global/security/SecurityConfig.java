d97b7a87d43c5d99dbd4b8e330b5f637cec606cb
Gooraeng <birdyoon1998@gmail.com>
2025-09-30 19:14:53 +0900

    fix: actuator 관련 엔드포인트 시큐리티 수정

--- back/src/main/java/com/back/global/security/SecurityConfig.java
+++ back/src/main/java/com/back/global/security/SecurityConfig.java
@@ -52,6 +52,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/users-auth/**", "/oauth2/**", "/login/oauth2/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .logout(logout -> logout
