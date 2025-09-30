92a94aa306a7e002e52e48dbcc0ff43bbd10549c
shihan00321 <shihan001@naver.com>
2025-09-30 10:20:14 +0900

    [Feat]: 선택한 투표 번호를 입력하는 요청 DTO 추가

---
+++ back/src/main/java/com/back/domain/poll/dto/VoteRequest.java
@@ -0,0 +1,15 @@
+package com.back.domain.poll.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+
+import java.util.List;
+
+/**
+ * 선택한 투표 번호
+ * 다중 선택 가능 [1, 2]
+ */
+public record VoteRequest(
+        @NotEmpty(message = "최소 하나의 선택지는 필수입니다")
+        List<Integer> choice
+) {
+}
