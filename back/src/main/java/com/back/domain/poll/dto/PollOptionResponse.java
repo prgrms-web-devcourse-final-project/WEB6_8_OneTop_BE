e532c8d52123ecd40e9012c56fcb33c85d79b040
shihan00321 <shihan001@naver.com>
2025-09-30 10:05:02 +0900

    [Feat]: 투표 게시글 본문에 보여줄 선택지 응답 DTO 구현

---
+++ back/src/main/java/com/back/domain/poll/dto/PollOptionResponse.java
@@ -0,0 +1,15 @@
+package com.back.domain.poll.dto;
+
+import java.util.List;
+
+/**
+ * 투표 선택지 응답 DTO
+ */
+public record PollOptionResponse(
+        List<VoteOption> options
+) {
+    public record VoteOption(
+            int index,
+            String text
+    ) {}
+}
