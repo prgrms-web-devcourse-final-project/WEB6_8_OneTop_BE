4b1e50cdcfe3811d7ddf1eb5c7a315910b442184
shihan00321 <shihan001@naver.com>
2025-10-01 14:31:54 +0900

    [Feat]: 댓글 좋아요 추가, 삭제 기능 추가
    # Conflicts:
    #	back/src/main/java/com/back/domain/comment/repository/CommentRepository.java

--- back/src/main/java/com/back/domain/like/repository/CommentLikeRepository.java
+++ back/src/main/java/com/back/domain/like/repository/CommentLikeRepository.java
@@ -2,6 +2,7 @@ package com.back.domain.like.repository;
 
 import com.back.domain.like.entity.CommentLike;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -15,4 +16,10 @@ import java.util.Set;
 public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
     @Query("SELECT cl.comment.id FROM CommentLike cl WHERE cl.user.id = :userId AND cl.comment.id IN :commentIds")
     Set<Long> findLikedCommentsIdsByUserAndCommentIds(@Param("userId") Long userId, @Param("commentIds") Set<Long> commentIds);
+
+    boolean existsByCommentIdAndUserId(Long commentId, Long userId);
+
+    @Modifying
+    @Query("DELETE FROM CommentLike cl WHERE cl.comment.id = :commentId AND cl.user.id = :userId")
+    int deleteByCommentIdAndUserId(@Param("commentId") Long commentId, @Param("userId") Long userId);
 }
