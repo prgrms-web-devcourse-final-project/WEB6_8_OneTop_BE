06cafe4040a1662b4d1e80aa8ffd62879897ba8f
Gooraeng <birdyoon1998@gmail.com>
2025-10-08 12:17:11 +0900

    fix: If not exists 구문 추가

--- back/src/main/resources/db/migration/V2__add_representative_to_scenarios.sql
+++ back/src/main/resources/db/migration/V2__add_representative_to_scenarios.sql
@@ -1,4 +1,4 @@
 -- ========================================
 -- 시나리오 대표 여부 컬럼 추가
 -- ========================================
-ALTER TABLE scenarios ADD COLUMN representative BOOLEAN DEFAULT FALSE NOT NULL;
+ALTER TABLE scenarios ADD COLUMN IF NOT EXISTS representative BOOLEAN DEFAULT FALSE NOT NULL;
